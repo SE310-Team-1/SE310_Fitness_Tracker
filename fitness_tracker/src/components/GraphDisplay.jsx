674a3537a531b459e41cc08dc1fe0d657e1b40ab
Ben-G-Man <bhum783@aucklanduni.ac.nz>
2024-08-20 00:25:40 +1200

    feat - Added dynamic graph generation

--- fitness_tracker/src/components/GraphDisplay.jsx
+++ fitness_tracker/src/components/GraphDisplay.jsx
@@ -1,28 +1,42 @@
 import GenerateGraph from './GraphGenerator';
-
-const tempData = [{date: 'Monday', volume1: 400, volume2: 900, amt: 2400}, 
-    {date: 'Tuesday', volume1: 200, volume2: 2400, amt: 2400},
-    {date: 'Wednesday', volume1: 300, volume2: 1100, amt: 2400},
-    {date: 'Thursday', volume1: 100, volume2: 800, amt: 2400},
-    {date: 'Friday', volume1: 100, volume2: 600, amt: 2400}];
+import { Fragment } from 'react';
 
 function GraphDisplay() {
-    var today = DateToString(new Date());
+    let endDate = DateToString(new Date());
+    let startDate = new Date();
+    startDate.setDate(startDate.getDate() - 6);
+    let tempData = FetchPeriod(startDate, new Date());
 
     return (
-        <div>        
+        <Fragment>        
             <GenerateGraph data={tempData}/>
-            <input id="dateSelector" type="date" max={today}></input>
+            <input id="dateSelector" type="date" max={endDate}></input>
+            <select>
+                <option>Exercise 1</option>
+                <option>Exercise 2</option>
+                <option>Exercise 3</option>
+                <option>Exercise 4</option>
+            </select>
             <button onClick={() => RefreshGraph(document.getElementById('dateSelector').value)}>CLICK ME</button>
-        </div>
+        </Fragment>
     )
 }
 
+function FetchPeriod(startDate, endDate) {
+    let data = [];
+
+    for (let nextDate = startDate; nextDate <= endDate; nextDate.setDate(nextDate.getDate() + 1)) {
+        data.push({date: DateToString(nextDate), score: Math.floor(Math.random() * 2400), amt: 2400});
+    }
+
+    return data;
+}
+
 function DateToString(date) {
-    var day = String(date.getDate()).padStart(2, '0');
+    let day = String(date.getDate()).padStart(2, '0');
     /* NOTE: Month indexes from 0, hence we add 1 to the month. */
-    var month = String(date.getMonth() + 1).padStart(2, '0');
-    var year = date.getFullYear();
+    let month = String(date.getMonth() + 1).padStart(2, '0');
+    let year = date.getFullYear();
     return year + "-" + month + "-" + day;
 }
 
--- fitness_tracker/src/components/GraphGenerator.jsx
+++ fitness_tracker/src/components/GraphGenerator.jsx
@@ -1,10 +1,17 @@
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Brush, Legend, Tooltip } from 'recharts';
 
+function GenerateLine() {
+    return (
+        <Line type="monotone" dataKey="volume2" stroke="#afafaf" strokeWidth={3} />
+    )
+}
+
+var generatedLine = GenerateLine();
+
 function GenerateGraph({ data }) {
     return (
-        <LineChart width={400} height={400} data={data}>
-            <Line type="monotone" dataKey="volume1" stroke="#8884d8" strokeWidth={3} />
-            <Line type="monotone" dataKey="volume2" stroke="#afafaf" strokeWidth={3} />
+        <LineChart id="graph" width={1000} height={400} data={data}>
+            <Line type="monotone" dataKey="score" stroke="#8884d8" strokeWidth={3} />
             <CartesianGrid stroke="#ccc" />
             <XAxis dataKey="date" />
             <YAxis/>
