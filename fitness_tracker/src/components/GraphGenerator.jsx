8f9d9f3ba0ef20bef8d534aadf89102f75b2d845
Ben-G-Man <bhum783@aucklanduni.ac.nz>
2024-08-20 20:47:42 +1200

    feat - Implemented responsive progress graph, does not currently fetch information from database

--- fitness_tracker/src/components/GraphGenerator.jsx
+++ fitness_tracker/src/components/GraphGenerator.jsx
@@ -1,23 +1,42 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Brush, Legend, Tooltip } from 'recharts';
+import {
+    ResponsiveContainer,
+    LineChart,
+    Line,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+} from "recharts";
 
-function GenerateLine() {
-    return (
-        <Line type="monotone" dataKey="volume2" stroke="#afafaf" strokeWidth={3} />
-    )
-}
+/* 
+    GenerateGraph() returns a Recharts ResponsiveContainer wrapping a line chart representing the
+    data set passed in the { data } parameter.
 
-var generatedLine = GenerateLine();
+    Parameters
+        data:   An array of objects in which each object contains an attribute for date and score.
 
+    Data Format:
+        data:   [obj1, obj2, ..., objN]
+        obj:    {date: "yyyy-mm-dd", score: num}
+        E.g.:   [{date: "1970-01-01", score: 100}, {date: "1970-01-02", score: 120}]
+*/
 function GenerateGraph({ data }) {
     return (
-        <LineChart id="graph" width={1000} height={400} data={data}>
-            <Line type="monotone" dataKey="score" stroke="#8884d8" strokeWidth={3} />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="date" />
-            <YAxis/>
-            <Tooltip />
-        </LineChart>
-    )
+        <ResponsiveContainer width="100%" height={300}>
+            <LineChart data={data}>
+                <Line
+                    type="monotone"
+                    dataKey="score"
+                    stroke="#8884d8"
+                    strokeWidth={3}
+                />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+            </LineChart>
+        </ResponsiveContainer>
+    );
 }
 
-export default GenerateGraph
+export default GenerateGraph;
