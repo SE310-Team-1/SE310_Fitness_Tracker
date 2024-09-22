2327e663a64854aaafb79085edbf0d5abcf03b3c
clen678 <126838584+clen678@users.noreply.github.com>
2024-09-22 19:16:37 +1200

    setting up react routes and login/sign up page (#76)

--- fitness_tracker/src/App.js
+++ fitness_tracker/src/App.js
@@ -3,6 +3,11 @@ import { useState, useEffect } from 'react';
 import TempWorkoutDisplay from './components/TempWorkoutDisplay';
 import TabDisplay from './components/TabDisplay';
 import GraphDisplay from './components/GraphDisplay';
+import Dashboard from './pages/Dashboard';
+import axios from 'axios';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
 // State for storing workouts
@@ -22,23 +27,31 @@ function App() {
       console.log(workouts)
   }
 
+  // This interceptor adds the Authorization header to all requests if a token is present in local storage
+  axios.interceptors.request.use ( (config) => {
+    const token = localStorage.getItem('token');
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+  });
+
   // Fetch workouts when component mounts
   useEffect(() => {
     fetchWorkouts()
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        FiTrack
-      </header>
-      <GraphDisplay />
-
-      <TabDisplay/>
-
-      {/* <TempWorkoutDisplay data={workouts}/> */}
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* If the user is logged in, redirect them to the dashboard, otherwise redirect them to the login page. This way
+            already logged in users won't have to keep re-loggin in. */}
+      <Route path="/" element={localStorage.getItem("token") ? <Navigate to="/dashboard" replace={true} /> : <Navigate to="/login" replace={true} />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="dashboard" element={
+          <Dashboard />
+          } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ fitness_tracker/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import LoginForm from "../components/LoginForm";
+
+export default function Login() {
+    return (
+        <div className="fixed h-full w-full flex justify-center items-center">
+            <LoginForm />
+        </div>
+    );
+}
