6e5eda5e0e47f6bb9b672c0d46563391d06aef92
clen678 <126838584+clen678@users.noreply.github.com>
2024-09-26 19:21:56 +1200

    Feature/login auth (#78)
    * setting up react routes and login/sign up page
    
    * adding login auth
    
    * fixing login and singup popups
    
    * Delete server/.env

--- fitness_tracker/src/components/AuthForm.jsx
+++ fitness_tracker/src/components/AuthForm.jsx
@@ -1,30 +1,41 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 // This component is a reusable form that can be used for both the login and signup pages. This was done to reduce code duplication.
 export default function AuthForm({ endpoint, title, buttonText, redirectTitleText, redirectLink, redirectText }) {
     const labelStyle = "text-2xl";
     const inputStyle = "border-2 w-64 h-8 p-2 rounded-lg mb-4 mt-2 border-primary focus:border-primary-highlight focus:outline-none";
 
-    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     function handleSubmit(e) {
         e.preventDefault();
+        console.log("Submitting form with data:", { username, password });
         axios.post(endpoint, {
-            email: email,
+            username: username,
             password: password
+        }, {
+            withCredentials: true
         }).then(response => {
-            if (response.data.success) {
-                // Store the returned auth token in local storage so that it can be easily accessed throughout the frontend
-                localStorage.setItem("token", response.data.token);
+            console.log("response is:", response);
+            if (response.data.success || response.status === 200 || response.status === 201) {
+                console.log("Successfully logged in or signed up!");
                 window.location.href = "/dashboard";
             } else {
-                alert("Invalid email or password!");
+                alert("Invalid username or password!");
             }
         }).catch(error => {
-            console.log(error);
+            if (error.response.status === 409) {
+                alert("User already exists!");
+            } if (error.response.status === 401) {
+                alert("Invalid username or password!");
+            }else {
+                console.log("An error occurred while trying to log in or sign up.");
+                console.log(error);
+            }
         });
     }
 
@@ -32,8 +43,8 @@ export default function AuthForm({ endpoint, title, buttonText, redirectTitleTex
         <form className="flex flex-col border-2 items-center border-primary-dark w-80 p-4 rounded-xl bg-blue-100">
             <h2 className="text-4xl font-bold text-center mb-4">{title}</h2>
             <div className="flex flex-col">
-                <label className={labelStyle} htmlFor="email">Email</label>
-                <input className={inputStyle} required type="email" onChange={(e) => { setEmail(e.target.value) }} value={email} />
+                <label className={labelStyle} htmlFor="username">Username</label>
+                <input className={inputStyle} required type="username" onChange={(e) => { setUsername(e.target.value) }} value={username} />
             </div>
             <div className="flex flex-col">
                 <label className={labelStyle} htmlFor="password">Password</label>
@@ -46,11 +57,12 @@ export default function AuthForm({ endpoint, title, buttonText, redirectTitleTex
     );
 }
 
+// Define prop types for the AuthForm component
 AuthForm.propTypes = {
-    endpoint: String,
-    title: String,
-    buttonText: String,
-    redirectTitleText: String,
-    redirectLink: String,
-    redirectText: String
+    endpoint: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    buttonText: PropTypes.string.isRequired,
+    redirectTitleText: PropTypes.string.isRequired,
+    redirectLink: PropTypes.string.isRequired,
+    redirectText: PropTypes.string.isRequired,
 };
--- fitness_tracker/src/pages/Dashboard.jsx
+++ fitness_tracker/src/pages/Dashboard.jsx
@@ -1,7 +1,32 @@
 import GraphDisplay from "../components/GraphDisplay";
 import TabDisplay from "../components/TabDisplay";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Dashboard = () => {
+    const [workouts, setWorkouts] = useState([]);
+    const [userId, setUserId] = useState("");
+
+    // fetch all workouts from the server
+    useEffect(() => {
+        console.log("useEffect called");
+        axios
+            .get("http://localhost:4001/workout/workouts", { withCredentials: true, })
+            .then((response) => {
+                console.log("API response:", response.data);
+                setUserId(response.data[0].user_id);
+                console.log("userId is:", response.data[0].user_id);
+
+                setWorkouts(response.data);
+                console.log("workouts are:", response.data);
+            })
+            .catch((error) => {
+                // If the user is not logged in (due to directly accessing dashboard path or token expiring), redirect to the login page
+                // window.location.href = "/login";
+                console.error("Not logged in ", error);
+            });
+    }, []);
+
     return (  
         <div className="App">
             <header className="App-header">
