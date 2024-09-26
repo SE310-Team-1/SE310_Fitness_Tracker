d4c74989197a57ad3f26a770311a0d3fe134860d
Taks07 <yutakaoishi7@gmail.com>
2024-09-26 20:30:32 +1200

    Changed some auth path stuff

--- fitness_tracker/src/pages/Dashboard.jsx
+++ fitness_tracker/src/pages/Dashboard.jsx
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 const Dashboard = () => {
     const [workouts, setWorkouts] = useState([]);
-    const [userId, setUserId] = useState("");
 
     // fetch all workouts from the server
     useEffect(() => {
@@ -13,16 +12,13 @@ const Dashboard = () => {
         axios
             .get("http://localhost:4001/workout/workouts", { withCredentials: true, })
             .then((response) => {
-                console.log("API response:", response.data);
-                setUserId(response.data[0].user_id);
-                console.log("userId is:", response.data[0].user_id);
 
                 setWorkouts(response.data);
                 console.log("workouts are:", response.data);
             })
             .catch((error) => {
-                // If the user is not logged in (due to directly accessing dashboard path or token expiring), redirect to the login page
-                // window.location.href = "/login";
+                // If the user is not logged in (due to directly accessing dashboard path or cookie expiring), redirect to the login page
+                window.location.href = "/login";
                 console.error("Not logged in ", error);
             });
     }, []);
