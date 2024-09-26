e69ed32487c5241137f5a242b0540ef33ea0b3fe
Taks07 <yutakaoishi7@gmail.com>
2024-09-26 19:31:59 +1200

    REmoved tailwind styles from AuthForm and created a module.css file for it

--- fitness_tracker/src/components/AuthForm.jsx
+++ fitness_tracker/src/components/AuthForm.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import PropTypes from "prop-types";
+import styles from "../module_CSS/AuthForm.module.css"
 
 // This component is a reusable form that can be used for both the login and signup pages. This was done to reduce code duplication.
 export default function AuthForm({ endpoint, title, buttonText, redirectTitleText, redirectLink, redirectText }) {
-    const labelStyle = "text-2xl";
-    const inputStyle = "border-2 w-64 h-8 p-2 rounded-lg mb-4 mt-2 border-primary focus:border-primary-highlight focus:outline-none";
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -40,19 +39,19 @@ export default function AuthForm({ endpoint, title, buttonText, redirectTitleTex
     }
 
     return (
-        <form className="flex flex-col border-2 items-center border-primary-dark w-80 p-4 rounded-xl bg-blue-100">
-            <h2 className="text-4xl font-bold text-center mb-4">{title}</h2>
-            <div className="flex flex-col">
-                <label className={labelStyle} htmlFor="username">Username</label>
-                <input className={inputStyle} required type="username" onChange={(e) => { setUsername(e.target.value) }} value={username} />
+        <form className={}>
+            <h2 className={}>{title}</h2>
+            <div className={}>
+                <label className={} htmlFor="username">Username</label>
+                <input className={} required type="username" onChange={(e) => { setUsername(e.target.value) }} value={username} />
             </div>
-            <div className="flex flex-col">
-                <label className={labelStyle} htmlFor="password">Password</label>
-                <input className={inputStyle} required type="password" onChange={(e) => { setPassword(e.target.value) }} value={password} />
+            <div className={}>
+                <label className={} htmlFor="password">Password</label>
+                <input className={} required type="password" onChange={(e) => { setPassword(e.target.value) }} value={password} />
             </div>
-            <button className="w-60 bg-primary text-white text-button p-2 rounded-lg mt-4" onClick={(e) => handleSubmit(e)}>{buttonText}</button>
-            <p className="text-xl mt-4">{redirectTitleText}</p>
-            <Link to={redirectLink} className="text-primary-highlight text-xl">{redirectText}</Link>
+            <button className={} onClick={(e) => handleSubmit(e)}>{buttonText}</button>
+            <p className={}>{redirectTitleText}</p>
+            <Link to={redirectLink} className={}>{redirectText}</Link>
         </form>
     );
 }
