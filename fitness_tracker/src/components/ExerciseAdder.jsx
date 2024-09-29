0a360eaa3b54f33048a2403adcd2909a1b7064ed
Taks07 <yutakaoishi7@gmail.com>
2024-09-29 23:22:30 +1300

    Fixed swapping new exercise

--- fitness_tracker/src/components/ExerciseAdder.jsx
+++ fitness_tracker/src/components/ExerciseAdder.jsx
@@ -4,34 +4,33 @@ import styles from '../module_CSS/ExerciseLogger.module.css'
 
 const ExerciseAdder = ({exerciseList, addExercise }) => {
     const [selectedExercise, setSelectedExercise] = useState(exerciseList[0])
+
     
     return (
-        <Fragment>
-            <tr>
-                <td>
-                    <select value={selectedExercise} onChange={(e) => setSelectedExercise(e.target.value)}>
-                        {exerciseList.map(exercise => (
-                            <option key={exercise.id} value={exercise}>{exercise.name}</option>
-                        ))}
-                    </select>
-                </td>
-                <td>
-                    {selectedExercise.weight}
-                </td>
-                <td>
-                    {selectedExercise.reps}
-                </td>
-                <td>
-                    {selectedExercise.setsGoal}
-                </td>
-                <td>
-                    0
-                </td>
+        <tr>
+            <td>
+                <select onChange={(e) => setSelectedExercise(exerciseList.find(exercise => exercise.name === e.target.value))}>
+                    {exerciseList.map(exercise => (
+                        <option key={exercise.name} value={exercise.name}>{exercise.name}</option>
+                    ))}
+                </select>
+            </td>
+            <td>
+                {selectedExercise.weight}
+            </td>
+            <td>
+                {selectedExercise.reps}
+            </td>
+            <td>
+                {selectedExercise.setsGoal}
+            </td>
+            <td>
+                0
+            </td>
 
-                <td><button className={`${buttons.button} ${buttons.addButton}`}>Delete</button></td>
-                <td><button className={`${buttons.button} ${styles.cancelButton}`} onClick={() => addExercise(selectedExercise)}>Add</button></td>
-            </tr>
-        </Fragment>
+            <td><button className={`${buttons.button} ${buttons.editButton}`}>Edit</button></td>
+            <td><button className={`${buttons.button} ${styles.addButton}`} onClick={() => addExercise(selectedExercise)}>Add</button></td>
+        </tr>
     )
 }
 
