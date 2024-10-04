64ebaec7704a36aa92308b9917a4148cdae5d5cc
vonner04 <randyleean092m@gmail.com>
2024-10-04 16:00:27 +1300

    feat: added muiMaterial icons to dependencies + developing front-end for workout history.

--- fitness_tracker/src/components/WorkoutHistoryDisplay.jsx
+++ fitness_tracker/src/components/WorkoutHistoryDisplay.jsx
@@ -1,9 +1,34 @@
 import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+  Collapse,
+  Typography,
+  Box,
+  List,
+  ListItem,
+} from "@mui/material";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
 import axios from "axios";
 
 function WorkoutHistoryDisplay() {
   const [workouts, setWorkouts] = useState([]);
   const [exerciseList, setExerciseList] = useState({});
+  // State to handle open/close status for each workout's exercises
+  const [openRows, setOpenRows] = useState({});
+
+  const handleRowClick = (id) => {
+    setOpenRows((prevOpenRows) => ({
+      ...prevOpenRows,
+      [id]: !prevOpenRows[id], // Toggle the open/close state for this row
+    }));
+  };
 
   const fetchExercises = async (workoutId) => {
     try {
@@ -11,6 +36,7 @@ function WorkoutHistoryDisplay() {
         `http://localhost:4001/workout/${workoutId}/exercises`,
         { withCredentials: true }
       );
+      console.log("Exercises for workout", workoutId, "are:", response.data);
       const exercisesData = response.data;
       setExerciseList((prevState) => ({
         ...prevState,
@@ -56,7 +82,83 @@ function WorkoutHistoryDisplay() {
   }, [exerciseList]);
   //TODO remove useEffect console logs
 
-  return <div></div>;
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Workout Date</TableCell>
+            <TableCell align="right">Score</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {workouts.map((workout) => (
+            <React.Fragment key={workout.id}>
+              <TableRow>
+                <TableCell>
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => handleRowClick(workout.id)}
+                  >
+                    {openRows[workout.id] ? (
+                      <KeyboardArrowUp />
+                    ) : (
+                      <KeyboardArrowDown />
+                    )}
+                  </IconButton>
+                </TableCell>
+                <TableCell>
+                  {new Date(workout.date).toLocaleDateString()}
+                </TableCell>
+                <TableCell align="right">{workout.score}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell
+                  style={{ paddingBottom: 0, paddingTop: 0 }}
+                  colSpan={3}
+                >
+                  <Collapse
+                    in={openRows[workout.id]}
+                    timeout="auto"
+                    unmountOnExit
+                  >
+                    <Box margin={1}>
+                      <Typography variant="h6" gutterBottom component="div">
+                        Exercises
+                      </Typography>
+                      <List>
+                        {exerciseList[workout.id] &&
+                        exerciseList[workout.id].length > 0 ? (
+                          exerciseList[workout.id].map((exercise, index) => (
+                            <ListItem key={index}>
+                              {exercise.name} - Sets Completed:
+                              {exercise.sets_completed}, Sets Goal:
+                              {exercise.setsGoal}, Reps:{exercise.reps}, Score:{" "}
+                              {Number(
+                                exercise.sets_completed / exercise.setsGoal
+                              ) *
+                                exercise.reps *
+                                exercise.weight}
+                            </ListItem>
+                          ))
+                        ) : (
+                          <ListItem>
+                            No exercises found for this workout.
+                          </ListItem>
+                        )}
+                      </List>
+                    </Box>
+                  </Collapse>
+                </TableCell>
+              </TableRow>
+            </React.Fragment>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default WorkoutHistoryDisplay;
